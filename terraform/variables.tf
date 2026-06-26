d40bbca5706f81382ac8c37901365e1a4c58623a
Raghunath Pradhan <pradhanraghunath91@gmail.com>
2026-06-26 22:57:55 +0530

    update region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region for the resources"
   type        = string
-  default     = "ap-southeast-1"
+  default     = "ap-south-1"
 }
 
 variable "environment" {
